134b990608e5c99908c2dfc1543b20de0a137f31
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-06-02 16:29:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
-
+function swapTheme()
+{
+	let container=document.querySelector("#app")
+	let btn=document.querySelector("#swap")
+	if(container.getAttribute("class")==="day")
+	{
+		container.setAttribute("class","night")
+		btn.setAttribute("class","button_night")
+	}
+	else
+	{
+		container.setAttribute("class","day")
+		btn.setAttribute("class","button_day")
+	}
+}
